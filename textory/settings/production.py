11e32084bae6f90405f0a7fe05c4e21ca506d9fa
yumin0 <yumin3991@gmail.com>
2017-11-04 19:35:38 +0800

    Update production

--- textory/settings/production.py
+++ textory/settings/production.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY','s^x1=muagzvdpc57ta6*pk$dc_#wvq$7jwg00c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['text-story.herokuapp.com']
+ALLOWED_HOSTS = ['textory.herokuapp.com']
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'youremail@gmail.com'
