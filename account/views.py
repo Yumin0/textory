1f3c2b4c129ed52d82a16fcef9ec7e5b907ff6fd
yumin0 <yumin3991@gmail.com>
2017-11-19 15:25:26 +0800

    register redirect to stories

--- account/views.py
+++ account/views.py
@@ -16,7 +16,7 @@ def register(request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
                 form.save()
-                return redirect('/')
+                return redirect('/stories')
     else:
         form = RegistrationForm()
 
