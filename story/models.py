863367c29a199e61dec997d4d46ad3b356131272
yumin0 <yumin3991@gmail.com>
2017-12-24 00:55:25 +0800

    uninstall taggit

--- story/models.py
+++ story/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 from django.contrib.auth.models import User
-from taggit.managers import TaggableManager
+
 
 class StoryAuthor(models.Model):
     """
